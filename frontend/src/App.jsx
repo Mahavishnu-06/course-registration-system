a0dbabe06003f5ed99d571c6f0a326a8a2806650
Mahavishnu06 <vishnujayabal06@gmail.com>
2026-09-06 08:42:32 +0530

    Fix frontend API URL import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-
+import API_URL from "./api";
 import StudentDashboard from "./StudentDashboard";
 import AdminDashboard from "./AdminDashboard";
 import AdminCourses from "./AdminCourses";
